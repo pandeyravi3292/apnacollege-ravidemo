e19d5758a21138f9197506c61d9f7e68467cb295
Ravi Pandey <pandeyravi3292@gmail.com>
2026-07-13 17:44:49 +0530

    adding files function.js and arrow_function.js using the branch js1

---
+++ JS/function.js
@@ -0,0 +1,14 @@
+function test() {
+    console.log("this is a test function");
+}
+
+// calling the function
+test();
+
+function sum(a,b){
+    console.log("adding two numbers");
+    let c = a+b;
+    console.log("sum is: "+c);
+}
+
+sum(10,20); // call by value/ params
